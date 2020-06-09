b87c58cb4131c9b2dd3043329eea30408ff2460f
Donster4 <66695206+Donster4@users.noreply.github.com>
2020-06-09 21:06:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ backup = require("discord-backup"),
 client = new Discord.Client(),
 settings = {
     prefix: "b!",
-    token: "YOURTOKEN"
+    token: "process.env.secret"
 };
 
 client.on("ready", () => {
